8095dbd81d4a21ea0753d6ed7661689798df67ec
costi <costi@L10-CostinelnN.central.local>
2019-11-23 23:34:46 +0100

    Added a config file
    Now working on getting the form fileds from the DB

--- Data/DB.py
+++ Data/DB.py
@@ -1,16 +1,19 @@
 import pymongo
+import Config
 
-client = pymongo.MongoClient("mongodb+srv://assetmanagement:o4kiykWtD40eilcU@training-ljw1a.gcp.mongodb.net/test?retryWrites=true&w=majority")
-db = client['assetmanagement']
-collection = db['workstations']
+client = pymongo.MongoClient(Config.db['connection_string'])
+db = client[Config.db['database']]
+workstations = db['workstations']
 
 class Insert:
 	def workstations(self, input_submit):
 		if input_submit is None:
 			print("Something went wrong")
 		else:
-			x = collection.insert_one(input_submit)
+			x = workstations.insert_one(input_submit)
 			print(input_submit)
-
-for x in collection.find():
+class Get:
+	def first_entry(self, workstations):
+		pass
+for x in workstations.find():
 	print(x)
